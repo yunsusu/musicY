53de052cfd64b8c2ab28c340042775106573e765
yunsusu <rladbstn1212@gmail.com>
2024-08-15 20:47:03 +0900

    fix

--- pages/spotify.jsx
+++ pages/spotify.jsx
@@ -1,36 +1,42 @@
 import { useEffect, useState } from "react";
 
-const clientId = "9cd43e0a85eb4c29bbd4900fddb31c8f";
-const redirectUri = "http://music-y.vercel.app";
-const scopes = [
-  "user-read-playback-state",
-  "user-modify-playback-state",
-  "streaming",
-  "user-read-currently-playing",
-  "playlist-read-private",
-  "user-library-read",
-].join(" ");
-
-const authUrl = `https://accounts.spotify.com/authorize?response_type=token&client_id=${clientId}&scope=${encodeURIComponent(
-  scopes
-)}&redirect_uri=${encodeURIComponent(redirectUri)}`;
-
 const SpotifyAuth = ({ onAuthSuccess }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [authUrl, setAuthUrl] = useState("");
 
   useEffect(() => {
-    const hash = window.location.hash;
-    const accessToken = new URLSearchParams(hash.substring(1)).get(
-      "access_token"
-    );
+    if (typeof window !== "undefined") {
+      const url = window.location.href; // 클라이언트에서만 window.location 사용
+      const clientId = "9cd43e0a85eb4c29bbd4900fddb31c8f";
+      const redirectUri = url;
+      const scopes = [
+        "user-read-playback-state",
+        "user-modify-playback-state",
+        "streaming",
+        "user-read-currently-playing",
+        "playlist-read-private",
+        "user-library-read",
+      ].join(" ");
 
-    if (!accessToken) {
-      window.location.href = authUrl;
-    } else {
-      setIsAuthenticated(true);
-      onAuthSuccess(accessToken);
+      const authUrl = `https://accounts.spotify.com/authorize?response_type=token&client_id=${clientId}&scope=${encodeURIComponent(
+        scopes
+      )}&redirect_uri=${encodeURIComponent(redirectUri)}`;
+
+      setAuthUrl(authUrl);
+
+      const hash = window.location.hash;
+      const accessToken = new URLSearchParams(hash.substring(1)).get(
+        "access_token"
+      );
+
+      if (!accessToken) {
+        window.location.href = authUrl;
+      } else {
+        setIsAuthenticated(true);
+        onAuthSuccess(accessToken);
+      }
     }
-  }, []);
+  }, [onAuthSuccess]);
 
   return <></>;
 };
